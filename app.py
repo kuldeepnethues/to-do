276522bf87b0e99a2b9490ac134fd779fee58e4e
kuldeepnethues <148373190+kuldeepnethues@users.noreply.github.com>
2023-10-20 19:41:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-# strike down a text
-def strike(text):
-    result = ''
-    for c in text:
-        result = result + c + '\u0336'
-    return result
-
 # Load data from CSV files
 issues_df = pd.read_csv("issues.csv", header=None, names=["Issue"])
 todos_df = pd.read_csv("todos.csv", header=None, names=["ToDo"])
@@ -15,6 +8,9 @@ todos_df = pd.read_csv("todos.csv", header=None, names=["ToDo"])
 # Initialize state for issues and todos
 issues = issues_df["Issue"].tolist()
 todos = todos_df["ToDo"].tolist()
+
+# Define a dictionary to store issue discussions
+issue_discussions = {}
 
 # Page selection
 page = st.sidebar.selectbox("Select a Page", ["Open Issues", "To-Do's"])
@@ -32,8 +28,20 @@ if page == "Open Issues":
     # Display open issues
     st.subheader("Issues:")
     for i, issue in enumerate(issues):
-        if st.checkbox(f"{i + 1}. {issue}", key=f"issue_{i}"):
-            issues[i] = strike(issue)
+        st.write(f"{i + 1}. {issue}")
+        
+        # Add a discussion button
+        if st.button(f"Open Discussion for Issue {i + 1}"):
+            discussion = st.text_area("Add a comment:")
+            if st.button("Add Comment"):
+                if i not in issue_discussions:
+                    issue_discussions[i] = []
+                issue_discussions[i].append(discussion)
+                st.text(f"Added Comment: {discussion}")
+        
+        # Mark Solved button
+        if st.button(f"Mark Solved for Issue {i + 1}"):
+            issues.remove(issue)
 
 elif page == "To-Do's":
     st.title("To-Do's")
@@ -48,7 +56,10 @@ elif page == "To-Do's":
     # Display to-do list
     st.subheader("To-Do List:")
     for i, todo in enumerate(todos):
-        if st.checkbox(f"{i + 1}. {todo}", key=f"todo_{i}"):
+        st.write(f"{i + 1}. {todo}")
+        
+        # Mark Solved button
+        if st.button(f"Mark Solved for To-Do {i + 1}"):
             todos.remove(todo)
 
 # Save data to CSV files
